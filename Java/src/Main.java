fd05f241d1fae61b64734541f485f92d8367a528
Wonho ha <haho013039@gmail.com>
2018-10-01 20:42:08 +0900

    Main use case

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
+        String map = "11111 11011 11100 00101 01011";
+        Tissue t = new Tissue(5, 5, map);
+        t.next();
+        t.print();
     }
 }
