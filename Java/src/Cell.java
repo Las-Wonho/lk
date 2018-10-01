4f78989af2d1ec5439f5412a121aad75c0564102
Wonho ha <haho013039@gmail.com>
2018-10-01 19:57:52 +0900

    Fix Cell bug
    몇가지 버그를 수정하였습니다.

--- Java/src/Cell.java
+++ Java/src/Cell.java
@@ -3,12 +3,14 @@ public class Cell {
     Cell(){
         state_of_life = true;
     }
-    Cell(boolean state){
-        state_of_life = true;
+
+    Cell(boolean state) {
+        state_of_life = state;
     }
+
     @Override
-    public String toString(){
-        if(state_of_life)
+    public String toString() {
+        if (state_of_life)
             return "live";
         return "dead";
     }
