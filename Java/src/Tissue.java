85eb8567337552263a0ed052e35ee50c5b877925
Wonho ha <haho013039@gmail.com>
2018-10-01 20:35:18 +0900

    Add Tissue constructor

--- Java/src/Tissue.java
+++ Java/src/Tissue.java
@@ -1,6 +1,19 @@
-public class Tissue {
+public class Tissue implements Printable{
+    int x, y;
+    int N;
+    Cell[][] cells;
 
-    Tissue(){
+    Tissue(int _n, int _m) {
+        N = 0;
+        x = _n;
+        y = _m;
+        cells = new Cell[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                cells[i][j] = new Cell();
+            }
+        }
+    }
 
     }
 }
