3b6ac0a12c20f4c4454fde0d7b150efc7d5bc125
Wonho ha <haho013039@gmail.com>
2018-10-01 20:35:53 +0900

    Add Tissue constructor parameter for string

--- Java/src/Tissue.java
+++ Java/src/Tissue.java
@@ -14,6 +14,24 @@ public class Tissue implements Printable{
             }
         }
     }
+    Tissue(int _n, int _m, String map) {
+        x = _n;
+        y = _m;
+        cells = new Cell[x][y];
+        String[] map_resource = map.split(" ");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                char c = map_resource[i].charAt(j);
+                if(c=='1'){
+                    cells[i][j] = new Cell(true);
+                }
+                else{
+                    cells[i][j] = new Cell(false);
+                }
+
+            }
+        }
+    }
 
     }
 }
