b6f672fd5327c56f8e5e479b20db11f6f85d21e6
Wonho ha <haho013039@gmail.com>
2018-10-03 06:08:35 +0900

    Fix bug
    입력이 잘못되는 경우 오류가 있었다.
    따라서 의도한 대로 작동하도록 로직을 변경하였다.

--- Java/src/InputLayer.java
+++ Java/src/InputLayer.java
@@ -20,13 +20,14 @@ public class InputLayer {
             if(input.equals("exit"))
                 break;
 
-            map += input + " ";
             if(y==0){
                 y=input.length();
             }else if(y != input.length()){
                 System.err.println("같은 줄 수가 아닙니다.");
                 System.err.println("다시 입력해주세요.");
+                continue;
             }
+            map += input + " ";
             x += 1;
         }
         return new Tissue(x,y,map);
