8c4be17c7fb3b8ffa9477129ef11acd56b6debe5
Wonho ha <haho013039@gmail.com>
2018-10-02 12:44:52 +0900

    Json test

--- JSONS_PASER/src/Main.java
+++ JSONS_PASER/src/Main.java
@@ -2,16 +2,30 @@ import com.google.gson.*;
 import com.sun.xml.internal.bind.AnyTypeAdapter;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Main {
-
     public static void main(String[] args) {
         // write your code here
-        Any json = new Any();
-        json.setValue("name","Shuvi")
-        .setValue("version","0.0.1")
-        .setValue("description","Electron製のYouTubeプレイヤー「Shuvi」");
-        System.out.println(json.json_value.get("name").string_value);
+        String _json = "{\n" +
+                "  \"name\": \"Shuvi\",\n" +
+                "  \"version\": \"0.0.1\",\n" +
+                "  \"description\": \"Electron製のYouTubeプレイヤー「Shuvi」\",\n" +
+                "  \"main\": \"src/bin/js/bin.js\",\n" +
+                "  \"scripts\": {\n" +
+                "    \"start\": \"electron .\",\n" +
+                "    \"build\": \"gulp\",\n" +
+                "    \"watch\": \"gulp watch\",\n" +
+                "    \"package:mac\": \"electron-packager . Shuvi --out=__release__ --platform=darwin --arch=x64 --icon=icons/icon.icns --overwrite\",\n" +
+                "    \"package:win\": \"electron-packager . Shuvi --out=__release__ --platform=win32 --arch=x64 --icon=icons/icon.ico --overwrite\",\n" +
+                "    \"package:linux\": \"electron-packager . Shuvi --out=__release__ --platform=linux --arch=x64 --icon=icons/icon.icns --overwrite\"\n" +
+                "  }\n"+
+                "  }";
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(_json);
+
+
+
     }
 }
