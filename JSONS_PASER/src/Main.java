add3d17c5c527fc357f62f64e9e83f582a0ad96f
Wonho ha <haho013039@gmail.com>
2018-10-02 14:10:54 +0900

    FIx style to coding convention

--- JSONS_PASER/src/Main.java
+++ JSONS_PASER/src/Main.java
@@ -4,6 +4,7 @@ import com.sun.xml.internal.bind.AnyTypeAdapter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,16 +22,15 @@ public class Main {
                 "    \"package:mac\": \"electron-packager . Shuvi --out=__release__ --platform=darwin --arch=x64 --icon=icons/icon.icns --overwrite\",\n" +
                 "    \"package:win\": \"electron-packager . Shuvi --out=__release__ --platform=win32 --arch=x64 --icon=icons/icon.ico --overwrite\",\n" +
                 "    \"package:linux\": \"electron-packager . Shuvi --out=__release__ --platform=linux --arch=x64 --icon=icons/icon.icns --overwrite\"\n" +
-                "  }\n"+
+                "  }\n" +
                 "  }";
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(_json);
 
 
-
         Map<String, _Any> json;
         json = new HashMap<>();
-        parse(json,element.getAsJsonObject());
+        parse(json, element.getAsJsonObject());
 
         System.out.println(json.get("version").get_string());
         System.out.println(json.get("name").get_string());
@@ -39,32 +39,36 @@ public class Main {
         System.out.println(json.get("scripts").get_json_Array().get("start").get_string());
 
     }
-    static void parse(Map<String,_Any> json,JsonElement e,String s){
-        if(e.isJsonPrimitive()){
+
+    static void parse(Map<String, _Any> json, JsonElement e, String s) {
+        if (e.isJsonPrimitive()) {
             JsonPrimitive jsonPrimitive = e.getAsJsonPrimitive();
-            if(jsonPrimitive.isNumber()) {
-                json.put(s,new _Any(e.getAsJsonPrimitive().getAsInt()));
-            }else if(jsonPrimitive.isString()){
-                json.put(s,new _Any(e.getAsJsonPrimitive().getAsString()));
-            }else if(jsonPrimitive.isBoolean()){
-                json.put(s,new _Any(e.getAsJsonPrimitive().getAsBoolean()));
+            if (jsonPrimitive.isNumber()) {
+                json.put(s, new _Any(e.getAsJsonPrimitive().getAsInt()));
+            } else if (jsonPrimitive.isString()) {
+                json.put(s, new _Any(e.getAsJsonPrimitive().getAsString()));
+            } else if (jsonPrimitive.isBoolean()) {
+                json.put(s, new _Any(e.getAsJsonPrimitive().getAsBoolean()));
             }
         }
-        if(e.isJsonObject()){
-            json.put(s,new _Any(new HashMap<String, _Any>()));
+        if (e.isJsonObject()) {
+            json.put(s, new _Any(new HashMap<String, _Any>()));
 
-            parse(json.get(s).get_json_Array(),e.getAsJsonObject());
+            parse(json.get(s).get_json_Array(), e.getAsJsonObject());
         }
-        if(e.isJsonArray()){
-            json.put(s,new _Any(new HashMap<String, _Any>()));
+        if (e.isJsonArray()) {
+            json.put(s, new _Any(new HashMap<String, _Any>()));
             JsonArray j = e.getAsJsonArray();
             for (int i = 0; i < j.size(); i++) {
-                parse(json.get(s).get_json_Array(), j.get(i),String.valueOf(i));
+                parse(json.get(s).get_json_Array(), j.get(i), String.valueOf(i));
             }
         }
     }
-    static void parse(Map<String,_Any> json,JsonObject j){
-        j.entrySet().iterator().forEachRemaining(stringJsonElementEntry -> {parse(json,stringJsonElementEntry.getValue(),stringJsonElementEntry.getKey());});
+
+    static void parse(Map<String, _Any> json, JsonObject j) {
+        j.entrySet().iterator().forEachRemaining(stringJsonElementEntry -> {
+            parse(json, stringJsonElementEntry.getValue(), stringJsonElementEntry.getKey());
+        });
 
     }
 }
