45ce10990323203f1d12bf3da4fbefad55ebde62
Wonho ha <haho013039@gmail.com>
2018-10-02 12:46:48 +0900

    Add _Ani class
    Just Wrapper class

---
+++ JSONS_PASER/src/_Any.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class _Any {
+    Object value;
+    _Any(Object v){
+        value = v;
+    }
+    Map<String, _Any> get_json_Array(){
+        try {
+            return Map.class.cast(value);
+        }catch (Exception e){
+            return HashMap.class.cast(value);
+        }
+    }
+    int get_int(){
+        try {
+            return (int)Integer.class.cast(value);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    double get_double(){
+        try {
+            return (double)Double.class.cast(value);
+        }catch (Exception e){
+        }
+        return 0;
+    }
+    String get_string(){
+        try {
+            return String.class.cast(value);
+        }catch (Exception e){
+        }
+        return "";
+    }
+}
