d791b3a8fd4d1b4534b8d94c8ad64a0ed995a661
Wonho ha <haho013039@gmail.com>
2018-10-02 14:12:03 +0900

    Fix style to coding convention

--- JSONS_PASER/src/_Any.java
+++ JSONS_PASER/src/_Any.java
@@ -3,35 +3,40 @@ import java.util.Map;
 
 public class _Any {
     Object value;
-    _Any(Object v){
+
+    _Any(Object v) {
         value = v;
     }
-    Map<String, _Any> get_json_Array(){
+
+    Map<String, _Any> get_json_Array() {
         try {
             return Map.class.cast(value);
-        }catch (Exception e){
+        } catch (Exception e) {
             return HashMap.class.cast(value);
         }
     }
-    int get_int(){
+
+    int get_int() {
         try {
-            return (int)Integer.class.cast(value);
-        }catch (Exception e){
+            return (int) Integer.class.cast(value);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return 0;
     }
-    double get_double(){
+
+    double get_double() {
         try {
-            return (double)Double.class.cast(value);
-        }catch (Exception e){
+            return (double) Double.class.cast(value);
+        } catch (Exception e) {
         }
         return 0;
     }
-    String get_string(){
+
+    String get_string() {
         try {
             return String.class.cast(value);
-        }catch (Exception e){
+        } catch (Exception e) {
         }
         return "";
     }
