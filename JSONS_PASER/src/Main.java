d2f1598dfee169dba7a196c8c47861c4096260d0
Wonho ha <haho013039@gmail.com>
2018-10-02 11:11:08 +0900

    Project init

--- JSONS_PASER/src/Main.java
+++ JSONS_PASER/src/Main.java
@@ -1,11 +1,17 @@
 import com.google.gson.*;
+import com.sun.xml.internal.bind.AnyTypeAdapter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        
+        Any json = new Any();
+        json.setValue("name","Shuvi")
+        .setValue("version","0.0.1")
+        .setValue("description","Electron製のYouTubeプレイヤー「Shuvi」");
+        System.out.println(json.json_value.get("name").string_value);
     }
 }
