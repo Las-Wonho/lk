d1eb10e035dffefb75e419aa83a7e3f826ad7724
Wonho ha <haho013039@gmail.com>
2018-10-02 11:08:54 +0900

    Change usecase

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,11 +1,11 @@
+import java.io.InputStream;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-
-        String map = "11111 11011 11100 00101 01011";
-        Tissue t = new Tissue(5, 5, map);
-        t.next();
-        t.print();
+        InputLayer layer = new InputLayer(System.in);
+        Tissue map = layer.DetectInout();
+        map.print();
     }
 }
