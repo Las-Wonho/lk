61aceb1c1fa7cde2e4923abee3592c4e2b841f10
Wonho ha <haho013039@gmail.com>
2018-10-03 06:03:13 +0900

    Delete main

--- JSONS_PASER/src/Main.java
+++ JSONS_PASER/src/Main.java
@@ -1,21 +1,6 @@
-import com.google.gson.*;
-import com.sun.xml.internal.bind.AnyTypeAdapter;
-
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        String _json = "";
-
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(_json);
-        Map<String, _Any> json;
-        json = new HashMap<>();
-        Json_Parse.parse(json, element.getAsJsonObject());
 
     }
 
